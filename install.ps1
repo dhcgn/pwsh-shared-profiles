f3b000f3b6625efafc8f3cc57d2e082836949e59
Daniel <dhcgn@users.noreply.github.com>
2022-12-23 14:47:27 +0100

    no cache

--- install.ps1
+++ install.ps1
@@ -101,13 +101,13 @@ function Setup-FolderAndFiles {
     }
 
     $url = "https://raw.githubusercontent.com/dhcgn/pwsh-shared-profiles/main/manager.ps1"
-    Invoke-WebRequest -Uri $url -OutFile (Join-Path $scriptsFolder "manager.ps1")
+    Invoke-WebRequest -Uri $url -OutFile (Join-Path $scriptsFolder "manager.ps1") -Headers @{"Cache-Control"="no-cache"}
 
     $url = "https://raw.githubusercontent.com/dhcgn/pwsh-shared-profiles/main/install.ps1"
-    Invoke-WebRequest -Uri $url -OutFile (Join-Path $scriptsFolder "install.ps1")
+    Invoke-WebRequest -Uri $url -OutFile (Join-Path $scriptsFolder "install.ps1") -Headers @{"Cache-Control"="no-cache"}
 
     $url = "https://raw.githubusercontent.com/dhcgn/pwsh-shared-profiles/main/README.md"
-    Invoke-WebRequest -Uri $url -OutFile (Join-Path $folder "README.md")
+    Invoke-WebRequest -Uri $url -OutFile (Join-Path $folder "README.md") -Headers @{"Cache-Control"="no-cache"}
 }
 
 function Check {
